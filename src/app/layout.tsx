a129e9e6fb3e9a4810abfde831ed4e49226788e7
Lawrence Ho <lawrence.jb.ho@gmail.com>
2023-06-09 17:11:22 -0700

    holding off on using app router because emotion (MUI) components do not support react server components yet

---
+++ src/app/layout.tsx
@@ -0,0 +1,46 @@
+import Head from "next/head";
+import { useMemo } from "react";
+import { AppProps } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import createEmotionCache from "../utils/createEmotionCache";
+import { themeSettings } from "./theme.ts";
+import "./index.css";
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+// later we'll modify this to its own file
+const theme = createTheme(themeSettings);
+// const theme = useMemo(() => createTheme(themeSettings), []);
+
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+export default function MyApp(props: MyAppProps) {
+  // If there's no emotionCache rendered by the server, use the clientSideEmotionCache
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <html>
+      <body>
+        <div className="app">
+          <CacheProvider value={emotionCache}>
+            <Head>
+              <meta
+                name="viewport"
+                content="initial-scale=1, width=device-width"
+              />
+            </Head>
+            <ThemeProvider theme={theme}>
+              {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+              <CssBaseline />
+              <Component {...pageProps} />
+            </ThemeProvider>
+          </CacheProvider>
+        </div>
+      </body>
+    </html>
+  );
+}
