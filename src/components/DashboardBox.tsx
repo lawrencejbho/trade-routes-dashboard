276efc8304efade82ae193d2818405432079fb32
Lawrence Ho <lawrence.jb.ho@gmail.com>
2023-06-12 16:44:17 -0700

    changed background color

--- src/components/DashboardBox.tsx
+++ src/components/DashboardBox.tsx
@@ -3,6 +3,8 @@ import { styled } from "@mui/system";
 
 const DashboardBox = styled(Box)(({ theme }) => ({
   backgroundColor: theme.palette.background.light,
+  // backgroundColor: "#2B3754",
+
   borderRadius: "1rem",
   boxShadow: "0.15rem 0.2rem 0.1rem rgba(0,0,0,.8)",
 }));
