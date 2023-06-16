7b0751de4f2bd3891d4391e51d7aa4ca38ccb9f0
Lawrence Ho <lawrence.jb.ho@gmail.com>
2023-06-15 20:43:11 -0700

    updating dates in sql queries to use the current timestamp

--- src/lib/getRecentTransactions.ts
+++ src/lib/getRecentTransactions.ts
@@ -16,13 +16,16 @@ export default async function recentTransactions(): Promise<any[]> {
     projectId: "square-big-query",
   });
 
+  const currentDate = new Date();
+  const formattedDate = currentDate.toISOString();
+
   /*
   - this query grabs all transactions for a given time frame and changes it to the PST timezone
   - also checks that the status is completed 
   */
   const sqlQuery = `SELECT totalMoney.amount, tipMoney.amount, DATETIME(createdAt, 'America/Los_Angeles') AS createdAt, cardDetails.card.cardholderName
   FROM \`square-big-query.my_states_dataset3.payments\`
-  WHERE createdAt BETWEEN TIMESTAMP("2023-06-01T00:00:00Z") AND TIMESTAMP("2023-06-12T07:41:03Z")
+  WHERE createdAt BETWEEN TIMESTAMP("2023-06-01T00:00:00Z") AND TIMESTAMP("${formattedDate}")
     AND status = 'COMPLETED'
   ORDER BY createdAt DESC
   LIMIT 50;
